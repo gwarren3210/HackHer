58b1a318cada98a541bbe613c26649a7e673358d
gwarren3210 <gavrielewarren@gmail.com>
2023-02-25 12:56:29 -0500

    second

---
+++ src/app2.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import './styles/App.css';
+
+
+const App = () => {
+
+  return (
+    {/* <div className="App">
+      <div className="container">
+        <div className="nav-container">
+            <button onClick={disconnect} id="nav-btn" className="cta-button connect-wallet-button">
+                Click Me!
+            </button>
+        </div>
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          <p className="sub-sub-text">(Please Connect to the Rinkeby Testnet)</p>
+          <form onSubmit={formMint}>
+                <input
+                  placeholder="Amount to Mint"
+                  type="number"
+                  value={amount}
+                  onChange={changeAmount}
+                  className="input-num"
+                />
+                <input
+                  type="submit"
+                  value="Mint"
+                  className="cta-button connect-wallet-button"
+                />
+            </form>
+        </div>
+        <div className="footer-container">
+        </div>
+      </div>
+    </div> */}
+  );
+};
+
+export default App;
